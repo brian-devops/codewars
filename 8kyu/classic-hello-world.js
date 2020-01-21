eb001cf1ed205a5cd5d7516c0def6c4a39a199ad
bryzey2019 <bryzey2019@gmail.com>
2020-01-22 10:38:33 +1100

    completed classic hello world kata using Class and static method

---
+++ 8kyu/classic-hello-world.js
@@ -0,0 +1,8 @@
+// Hello world using class - https://www.codewars.com/kata/57036f007fd72e3b77000023/train/javascript
+
+// Print "Hello World!" to the screen
+class Solution {
+  static main() {
+    console.log("Hello World!");
+  }
+}
