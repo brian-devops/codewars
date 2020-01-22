54e6ddb492efa1bf625258092809fb170033cc9f
bryzey2019 <bryzey2019@gmail.com>
2020-01-23 10:30:11 +1100

    completed sum the strings kata using Number to convert the values to integer and then String to convert the result back to a string

---
+++ 8kyu/sum-the-strings.js
@@ -0,0 +1,8 @@
+//Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+//https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+function sumStr(a, b) {
+  let result = Number(a) + Number(b);
+  return String(result);
+}
