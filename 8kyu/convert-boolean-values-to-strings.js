cfe4ad9fc1c40de6165587742decab81a06f7936
bryzey2019 <bryzey2019@gmail.com>
2020-01-25 19:32:58 +1100

    completed convert boolean to strings kata using ternary operator

---
+++ 8kyu/convert-boolean-values-to-strings.js
@@ -0,0 +1,9 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+// Long way
+// function boolToWord( bool ){
+//   return (bool ? 'Yes' : 'No');
+// }
+
+const boolToWord = bool => (bool ? "Yes" : "No");
