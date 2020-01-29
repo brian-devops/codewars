921b7c8403f0744bfbb75023d13ee1b3207f17cf
bryzey2019 <bryzey2019@gmail.com>
2020-01-30 06:48:16 +1100

    completed cockroach kata using Math.floor

--- 8kyu/sum-without-highest-and-lowest-number.js
+++ 8kyu/sum-without-highest-and-lowest-number.js
@@ -3,14 +3,26 @@ Sum all the numbers of the array (in F# and Haskell you get a list) except the h
 (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
 */
 
-function sumArray(array) {
-  let min = Math.min(...array);
-  let max = Math.max(...array);
+// function sumArray(array) {
+//   let min = Math.min(...array);
+//   let max = Math.max(...array);
 
-  for (var i = 0, count = 0; i <= array.length; i++) {
-    if (array[i] > min && array[i] < max) {
-      count += array[i];
-    }
-  }
-  return count;
+//   if (array === null) {
+//     return 0;
+//   }
+
+//   for (var i = 0, count = 0; i <= array.length; i++) {
+//     if (array[i] > min && array[i] < max) {
+//       count += array[i];
+//     }
+//   }
+//   return count;
+// }
+function sumArray(array) {
+  if (!Array.isArray(array)) return 0;
+
+  return array
+    .sort((a, b) => a - b)
+    .slice(1, array.length - 1)
+    .reduce((a, b) => a + b, 0);
 }
