d3442299a49cde8d72c3ab1d819d65da71fcdad1
bryzey2019 <bryzey2019@gmail.com>
2020-02-06 13:57:12 +1100

    completed multiples of 3 or 5 kata using a for loop and redeuce to sum each multiple of 3 and 5

---
+++ 6kyu/multiples-of-3-or-5.js
@@ -0,0 +1,12 @@
+function solution(number) {
+  let arr = [];
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr.reduce(
+    (accumulator, currentValue) => (accumulator += currentValue),
+    0
+  );
+}
