d08d129476269507c6372d3544015e472eca816d
bryzey2019 <bryzey2019@gmail.com>
2020-02-20 06:44:38 +1100

    completed using arrow and ternary

---
+++ 8kyu/feb/the-wide-mouthed-frog.js
@@ -0,0 +1,11 @@
+// Long
+// function mouthSize(animal) {
+//   if (animal === "alligator") {
+//     return "small";
+//   } else {
+//     return "wide";
+//   }
+// }
+
+const mouthSize = animal =>
+  animal.toLowerCase() === "alligator" ? "small" : "wide";
